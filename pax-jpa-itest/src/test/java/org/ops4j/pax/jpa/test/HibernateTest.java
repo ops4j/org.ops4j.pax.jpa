1d13295c233e8510b441b30194bb8eeef91d6588
Harald Wellmann <harald.wellmann@gmx.de>
2013-10-29 19:11:36 +0100

    [PAXJPA-11] wrapping javassist no longer required

--- pax-jpa-itest/src/test/java/org/ops4j/pax/jpa/test/HibernateTest.java
+++ pax-jpa-itest/src/test/java/org/ops4j/pax/jpa/test/HibernateTest.java
@@ -18,6 +18,7 @@
 package org.ops4j.pax.jpa.test;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 import static org.ops4j.pax.exam.CoreOptions.bundle;
 import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
 import static org.ops4j.pax.exam.CoreOptions.options;
@@ -26,60 +27,75 @@ import static org.ops4j.pax.exam.CoreOptions.systemProperty;
 import static org.ops4j.pax.exam.CoreOptions.wrappedBundle;
 import static org.ops4j.pax.jpa.test.TestConfiguration.regressionDefaults;
 
+import java.util.List;
+
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
 
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.ops4j.pax.exam.Configuration;
 import org.ops4j.pax.exam.Option;
 import org.ops4j.pax.exam.junit.PaxExam;
+import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
+import org.ops4j.pax.exam.spi.reactors.PerClass;
 import org.ops4j.pax.exam.util.Filter;
 import org.ops4j.pax.exam.util.PathUtils;
+import org.ops4j.pax.jpa.sample1.model.Author;
 import org.osgi.framework.BundleContext;
 
 @RunWith(PaxExam.class)
+@ExamReactorStrategy(PerClass.class)
 public class HibernateTest {
 
     @Inject
     private BundleContext bc;
 
     @Inject
-    @Filter(value = "(osgi.unit.name=library)", timeout = 1000000)
+    @Filter(value = "(osgi.unit.name=library)")
     private EntityManagerFactory emf;
 
     @Configuration
     public Option[] config() {
         return options(
             regressionDefaults(),
-            
+
             systemProperty("org.jboss.logging.provider").value("slf4j"),
-            systemPackages("javax.xml.stream; version=\"1.0.0\"", 
-                    "javax.xml.stream.events; version=\"1.0.0\"",
-                    "javax.xml.stream.util; version=\"1.0.0\""),
-            
-            
+            systemPackages("javax.xml.stream; version=\"1.0.0\"",
+                "javax.xml.stream.events; version=\"1.0.0\"",
+                "javax.xml.stream.util; version=\"1.0.0\""),
+
             bundle("reference:file:" + PathUtils.getBaseDir() + "/../pax-jpa/target/classes"), //
 
             mavenBundle("org.ops4j.pax.jdbc", "pax-jdbc").versionAsInProject(),
             mavenBundle("org.ops4j.pax.jpa.samples", "pax-jpa-sample1").versionAsInProject(),
             mavenBundle("org.apache.geronimo.specs", "geronimo-jpa_2.0_spec").versionAsInProject(),
             mavenBundle("org.apache.geronimo.specs", "geronimo-jta_1.1_spec").versionAsInProject(),
-            mavenBundle("org.apache.geronimo.specs", "geronimo-servlet_3.0_spec").versionAsInProject(),
+            mavenBundle("org.apache.geronimo.specs", "geronimo-servlet_3.0_spec")
+                .versionAsInProject(),
 
-            //mavenBundle("org.ops4j.pax.jpa", "pax-jpa-hibernate").versionAsInProject(),
-            bundle("reference:file:" + PathUtils.getBaseDir() + "/../pax-jpa-hibernate/target/classes"), //
+            // mavenBundle("org.ops4j.pax.jpa", "pax-jpa-hibernate").versionAsInProject(),
+            bundle("reference:file:" + PathUtils.getBaseDir()
+                + "/../pax-jpa-hibernate/target/classes"), //
             mavenBundle("org.hibernate", "hibernate-core").versionAsInProject(),
             mavenBundle("org.hibernate", "hibernate-entitymanager").versionAsInProject(),
-                
-            mavenBundle("org.apache.servicemix.bundles", "org.apache.servicemix.bundles.ant", "1.8.2_2"),    
-            mavenBundle("org.apache.servicemix.bundles", "org.apache.servicemix.bundles.antlr", "2.7.7_5"),    
-            mavenBundle("org.apache.servicemix.bundles", "org.apache.servicemix.bundles.dom4j", "1.6.1_5"),    
-            wrappedBundle(mavenBundle("org.javassist", "javassist", "3.18.1-GA")),
+
+            mavenBundle("org.apache.servicemix.bundles", "org.apache.servicemix.bundles.ant",
+                "1.8.2_2"),
+            mavenBundle("org.apache.servicemix.bundles", "org.apache.servicemix.bundles.antlr",
+                "2.7.7_5"),
+            mavenBundle("org.apache.servicemix.bundles", "org.apache.servicemix.bundles.dom4j",
+                "1.6.1_5"), //
+            mavenBundle("org.javassist", "javassist", "3.18.1-GA"),
             mavenBundle("com.fasterxml", "classmate", "0.5.4"),
+            
+            // not an OSGi bundle, see JANDEX-19
             wrappedBundle(mavenBundle("org.jboss", "jandex", "1.1.0.Final")),
+            
             mavenBundle("org.jboss.logging", "jboss-logging", "3.1.0.GA"),
             mavenBundle("org.hibernate.common", "hibernate-commons-annotations", "4.0.2.Final"),
 
@@ -94,4 +110,24 @@ public class HibernateTest {
         EntityManager em = emf.createEntityManager();
         assertNotNull(em);
     }
+
+    @Test
+    public void runCriteriaQuery() {
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        try {
+            assertNotNull(em);
+            CriteriaBuilder cb = em.getCriteriaBuilder();
+            CriteriaQuery<Author> q = cb.createQuery(Author.class);
+            q.from(Author.class);
+            TypedQuery<Author> typedQuery = em.createQuery(q);
+            List<Author> list = typedQuery.getResultList();
+            assertNotNull(list);
+            assertTrue(list.isEmpty());
+        }
+        finally {
+            em.getTransaction().rollback();
+        }
+    }
+
 }
